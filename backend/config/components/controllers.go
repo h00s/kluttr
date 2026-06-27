9f2dc3c9f87ef3aaee814f97f81f57646abaed4e
Krunoslav Husak <kruno@binel.hr>
2026-06-28 00:56:22 +0200

    Remove hello controller and service

--- backend/config/components/controllers.go
+++ backend/config/components/controllers.go
@@ -2,11 +2,8 @@ package components
 
 import (
 	"github.com/go-raptor/raptor/v4"
-	"github.com/h00s/kluttr/backend/app/controllers"
 )
 
 func Controllers() raptor.Controllers {
-	return raptor.Controllers{
-		&controllers.HelloController{},
-	}
+	return raptor.Controllers{}
 }
--- backend/config/components/services.go
+++ backend/config/components/services.go
@@ -2,11 +2,8 @@ package components
 
 import (
 	"github.com/go-raptor/raptor/v4"
-	"github.com/h00s/kluttr/backend/app/services"
 )
 
 func Services() raptor.Services {
-	return raptor.Services{
-		&services.HelloService{},
-	}
+	return raptor.Services{}
 }
